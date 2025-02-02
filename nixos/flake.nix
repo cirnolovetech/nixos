cde6dfb703367b3d61e943a3641ac02dfbe04705
Cirno <chillinwithmao@gmail.com>
2025-02-02 15:36:40 +0700

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,27 +1,31 @@
 {
-  description = "Nixos config flake";
+ description = "Flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+ inputs = {
+  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  home-manager = {
+   url = "github:nix-community/home-manager";
+   inputs.nixpkgs.follows = "nixpkgs";
   };
+ };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+ outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
+   system = "x86_64-linux";
+   pkgs = nixpkgs.legacyPackages.${system};
+  in
+   {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+     specialArgs = {inherit inputs;};
+     modules = [
+      ./configuration.nix
+     ];
     };
-  };
+
+    homeConfigurations.cirno = home-manager.lib.homeManagerConfiguration {
+     pkgs = nixpkgs.legacyPackages.${system};
+     modules = [ ./home-manager/home.nix ];
+    };
+   };
 }
