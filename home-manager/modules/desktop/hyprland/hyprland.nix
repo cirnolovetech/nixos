2db0df9580bbf289dfdb4c198084c17cac9edd0c
Cirno <chillinwithmao@gmail.com>
2025-03-08 00:17:42 +0700

    Update hyprland.nix

--- home-manager/modules/desktop/hyprland/hyprland.nix
+++ home-manager/modules/desktop/hyprland/hyprland.nix
@@ -52,10 +52,10 @@
   
   # Decoration
    decoration = {
-    rounding = 10;
+    rounding = 0;
     active_opacity = 1.0;
     inactive_opacity = 0.9;
-    fullscreen_opacity = 0.9;
+    fullscreen_opacity = 1.0;
    };
    
    # Animation
