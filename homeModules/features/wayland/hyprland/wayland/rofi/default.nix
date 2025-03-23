c926a4b89bd98ce8c1573c21899ba949d1699ea8
Cirno 🍌 <193801055+cirnolovetech@users.noreply.github.com>
2025-03-23 14:50:52 +0700

    Add files via upload

---
+++ homeModules/features/wayland/hyprland/wayland/rofi/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+
+ programs.rofi = {
+  enable = true;
+  package = pkgs.rofi-wayland;
+  plugins = with pkgs; [
+   rofi-emoji-wayland
+  ];
+  theme = ./theme.rasi;
+  extraConfig = {
+   display-drun = "Search:";
+   display-window = "Windows:";
+   display-emoji = "Emoji:";
+   drun-display-format = "{icon} {name}";
+   modi = "drun,run,window,emoji";
+   show-icons = true;
+  };
+ };
+ 
+}
