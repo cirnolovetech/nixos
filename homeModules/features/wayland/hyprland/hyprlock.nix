00cad1ef923a4d86b3acc2f175998841d6423bae
Cirno 🍌 <193801055+cirnolovetech@users.noreply.github.com>
2025-03-23 20:18:09 +0700

    Update hyprlock.nix

--- homeModules/features/wayland/hyprland/hyprlock.nix
+++ homeModules/features/wayland/hyprland/hyprlock.nix
@@ -8,20 +8,11 @@
    general = {
     grace = 1;
    };
-
    label = [
-    {
-     text = ''cmd[update:18000000] echo "$(date +'%-d %B %Y')"'';
-     font = 40;
-     position = "0, -150";
-     halign = "center";
-     valign = "top";
-    }
-       
     {
      text = ''cmd[update:1000] echo "$(date +'%H:%M')"'';
      font_size = 38;
-     position = "0, -80";
+     position = "0, -120";
      halign = "center";
      valign = "top";
     }
